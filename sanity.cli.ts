9c5c1bd940568255f7e5ad5a154e8bb41bbf6bb5
Bashar al-Idreesi <basharalidreesi@Bashars-MacBook-Pro-2.local>
2025-02-10 16:52:57 +0300

    First commit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,14 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
-  api: {
-    projectId: 'b845ek2k',
-    dataset: 'production'
-  },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
-})
+	api: {
+		projectId: "b845ek2k",
+		dataset: "production",
+	},
+	/**
+	 * Enable auto-updates for studios.
+	 * Learn more at https://www.sanity.io/docs/cli#auto-updates
+	 */
+	autoUpdates: true,
+	studioHost: "makana",
+});
